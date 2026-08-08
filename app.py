52dbb4aa5a33db817ca942253f12d5e4e004c520
shameemalungal <127572908+shameemalungal@users.noreply.github.com>
2026-08-08 13:01:29 +0530

    Disable Flask debug mode in production

--- app.py
+++ app.py
@@ -2,5 +2,11 @@ from app import create_app
 
 app = create_app()
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        debug=not app.config.get(
+            "IS_PRODUCTION",
+            False,
+        )
+    )
